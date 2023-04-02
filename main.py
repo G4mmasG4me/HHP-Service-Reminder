a7d17525db3bfed29d8dfaeed47b212f39de345f
GammasGame <domhough@hotmail.co.uk>
2023-04-02 06:26:48 +0100

    error handing and email content update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import datetime
 import smtplib
-import details
+
 import schedule
 import time
 import csv
@@ -9,23 +9,49 @@ import os
 
 from connect import mydb
 
+import details
+with open('email_template.html') as f:
+  email_template = f.read()
+
 mycursor = mydb.cursor(prepared=True)
 
-email_subject = 'Book your upcoming boiler service!'
-email_content = 'Book your upcoming boiler service with hothouse properties!'
+subject = 'Book your upcoming boiler service!'
 
 
-def send_email(receiver, ):
-  print('Start')
-  smtp_server = smtplib.SMTP('smtp.office365.com', 587)
-  print('SMTP Started')
-  smtp_server.ehlo() # Hostname to send for this command defaults to the fully qualified domain name of the local host.
-  smtp_server.starttls() #Puts connection to SMTP server in TLS mode
-  smtp_server.ehlo()
+def send_email(smtp_server, receiver, name, address, upcoming_service):
+  # generate email
+  body = email_template.format(name=name, service_date=upcoming_service, address=address)
+  msg = f'From: {details.sender}\nTo: {receiver}\nSubject: {subject}\nContent-Type: text/html;\n\n{body}'
 
-  smtp_server.login(details.sender, details.password)
-  print('Test')
-  smtp_server.sendmail(details.sender, receiver, email_content)
+
+  # tries to send email up to 5 times
+  for i in range(5):
+    try:
+      smtp_server.sendmail(details.sender, receiver, msg)
+      return True, None
+    except Exception as e:
+      pass
+  return False, e
+  
+
+
+def open_smtp():
+  for i in range(5): # try and connect up to 5 times
+    try:
+      smtp_server = smtplib.SMTP('smtp.office365.com', 587)
+      smtp_server.ehlo() # Hostname to send for this command defaults to the fully qualified domain name of the local host.
+      smtp_server.starttls() #Puts connection to SMTP server in TLS mode
+      smtp_server.ehlo()
+
+      smtp_server.login(details.sender, details.password)
+      return smtp_server, True, None # server object, pass, error
+    except Exception as e:
+      pass
+  return None, False, e # server object, pass, error
+
+
+def close_smtp(smtp_server):
+  smtp_server.quit()
 
 day_notice = 14
 sent_email_sql = 'INSERT INTO `sent_email` (customer_id) VALUES (%s)'
@@ -43,42 +69,61 @@ def main():
   values = (day_notice, day_notice)
   mycursor.execute(sql, values)
   result = mycursor.fetchall()
-  current_date = datetime.date.today()
-  for row in result:
-    customer_id = row[0]
-    name = row[1]
-    email = row[2]
-    address = row[3]
-    previous_service = row[4]
-    upcoming_service = row[5]
-    last_email_sent = row[6]
-    send_email(name, email, address, upcoming_service)
-    mycursor.execute(sent_email_sql, customer_id)
-    with open('email_logs.csv', 'a') as f:
-      writer = csv.writer(f)
-      time = datetime.utcnow().strftime('%Y-%m-%d, %H:%M:%S')
-      writer.writerow([time, customer_id, previous_service, upcoming_service, last_email_sent])
+  if result:
+    # connect to smtp server
+    smtp_server, success, error = open_smtp()
+    #  if connect, loop through mysql results
+    if success:
+      for row in result:
+        # collect row data
+        customer_id = row[0]
+        name = row[1]
+        email = row[2]
+        address = row[3]
+        previous_service = row[4]
+        upcoming_service = row[5]
+        last_email_sent = row[6]
+
+        # send email
+        success, error = send_email(smtp_server, name, email, address, upcoming_service)
+
+        # execute 
+        if success:
+          mycursor.execute(sent_email_sql, customer_id)
+        with open('email_logs.csv', 'a') as f:
+          writer = csv.writer(f)
+          time = datetime.utcnow().strftime('%Y-%m-%d, %H:%M:%S')
+          writer.writerow([time, customer_id, previous_service, upcoming_service, last_email_sent, error])
   with open('running_logs.csv') as f:
     writer = csv.writer(f)
     time = datetime.utcnow().strftime('%Y-%m-%d, %H:%M:%S')
-    writer.writerow([time, len(result)])
+    writer.writerow([time, len(result), error])
 
 
-def runner():
+def run():
+  # create running logs csv if it doesn't exist
   if not os.path.exists('running_logs.csv'):
     with open('running_logs.csv', 'w') as f:
       writer = csv.writer(f)
-      writer.writerow(['DateTime (UTC)', 'Emails Sent'])
+      writer.writerow(['DateTime (UTC)', 'Emails To Be Sent', 'Error'])
 
+  # create email logs csv if it doesnt' exist
   if not os.path.exists('email_logs.csv'):
     with open('email_logs.csv', 'w') as f:
       writer = csv.writer(f)
-      writer.writerow(['DateTime (UTC)', 'Customer ID', 'Previous Service (UTC)', 'Upcoming Service (UTC)', 'Last Email Sent (UTC)'])
+      writer.writerow(['DateTime (UTC)', 'Customer ID', 'Previous Service (UTC)', 'Upcoming Service (UTC)', 'Last Email Sent (UTC)', 'Error'])
     
-
+  # runs schedule
   while True:
     schedule.run_pending()
     time.sleep(1)
 
+smtp_server, success, error = open_smtp()
+success, error = send_email(smtp_server, 'domhough@hotmail.co.uk', 'Mr Dominic Rollason-Hough', '101 Castlecroft Road, WV3 8BY, West Midlands', '14/02/2004')
+if success:
+  print('Success')
+else:
+  print(error)
+quit()
 schedule.every().day.at('09:00').do(main)
-runner()
+run()
